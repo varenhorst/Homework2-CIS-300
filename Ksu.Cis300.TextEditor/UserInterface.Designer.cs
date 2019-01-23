908ea6a6578ef999b9dd95263cc186c0ba28751a
Rod Howell <rhowell@ksu.edu>
2019-01-23 15:24:09 -0600

    Lab 4 solution.

--- Ksu.Cis300.TextEditor/UserInterface.Designer.cs
+++ Ksu.Cis300.TextEditor/UserInterface.Designer.cs
@@ -35,6 +35,9 @@
             this.uxEditBuffer = new System.Windows.Forms.TextBox();
             this.uxOpenDialog = new System.Windows.Forms.OpenFileDialog();
             this.uxSaveDialog = new System.Windows.Forms.SaveFileDialog();
+            this.uxEncrypt = new System.Windows.Forms.ToolStripMenuItem();
+            this.uxEncryptWithString = new System.Windows.Forms.ToolStripMenuItem();
+            this.uxEncryptWithStringBuilder = new System.Windows.Forms.ToolStripMenuItem();
             this.uxMenuBar.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -42,7 +45,8 @@
             // 
             this.uxMenuBar.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.uxMenuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.uxFile});
+            this.uxFile,
+            this.uxEncrypt});
             this.uxMenuBar.Location = new System.Drawing.Point(0, 0);
             this.uxMenuBar.Name = "uxMenuBar";
             this.uxMenuBar.Size = new System.Drawing.Size(583, 33);
@@ -61,14 +65,14 @@
             // uxOpen
             // 
             this.uxOpen.Name = "uxOpen";
-            this.uxOpen.Size = new System.Drawing.Size(185, 30);
+            this.uxOpen.Size = new System.Drawing.Size(252, 30);
             this.uxOpen.Text = "Open . . .";
             this.uxOpen.Click += new System.EventHandler(this.uxOpen_Click);
             // 
             // uxSaveAs
             // 
             this.uxSaveAs.Name = "uxSaveAs";
-            this.uxSaveAs.Size = new System.Drawing.Size(185, 30);
+            this.uxSaveAs.Size = new System.Drawing.Size(252, 30);
             this.uxSaveAs.Text = "Save As . . .";
             this.uxSaveAs.Click += new System.EventHandler(this.uxSaveAs_Click);
             // 
@@ -78,11 +82,35 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.uxEditBuffer.Location = new System.Drawing.Point(12, 36);
+            this.uxEditBuffer.MaxLength = 0;
             this.uxEditBuffer.Multiline = true;
             this.uxEditBuffer.Name = "uxEditBuffer";
             this.uxEditBuffer.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.uxEditBuffer.Size = new System.Drawing.Size(559, 474);
             this.uxEditBuffer.TabIndex = 1;
+            // 
+            // uxEncrypt
+            // 
+            this.uxEncrypt.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uxEncryptWithString,
+            this.uxEncryptWithStringBuilder});
+            this.uxEncrypt.Name = "uxEncrypt";
+            this.uxEncrypt.Size = new System.Drawing.Size(83, 29);
+            this.uxEncrypt.Text = "Encrypt";
+            // 
+            // uxEncryptWithString
+            // 
+            this.uxEncryptWithString.Name = "uxEncryptWithString";
+            this.uxEncryptWithString.Size = new System.Drawing.Size(252, 30);
+            this.uxEncryptWithString.Text = "With String";
+            this.uxEncryptWithString.Click += new System.EventHandler(this.uxEncryptWithString_Click);
+            // 
+            // uxEncryptWithStringBuilder
+            // 
+            this.uxEncryptWithStringBuilder.Name = "uxEncryptWithStringBuilder";
+            this.uxEncryptWithStringBuilder.Size = new System.Drawing.Size(252, 30);
+            this.uxEncryptWithStringBuilder.Text = "With StringBuilder";
+            this.uxEncryptWithStringBuilder.Click += new System.EventHandler(this.uxEncryptWithStringBuilder_Click);
             // 
             // UserInterface
             // 
@@ -110,6 +138,9 @@
         private System.Windows.Forms.TextBox uxEditBuffer;
         private System.Windows.Forms.OpenFileDialog uxOpenDialog;
         private System.Windows.Forms.SaveFileDialog uxSaveDialog;
+        private System.Windows.Forms.ToolStripMenuItem uxEncrypt;
+        private System.Windows.Forms.ToolStripMenuItem uxEncryptWithString;
+        private System.Windows.Forms.ToolStripMenuItem uxEncryptWithStringBuilder;
     }
 }
 
--- Ksu.Cis300.TextEditor/UserInterface.cs
+++ Ksu.Cis300.TextEditor/UserInterface.cs
@@ -75,5 +75,73 @@ namespace Ksu.Cis300.TextEditor
         {
             MessageBox.Show("The following error occurred: " + e);
         }
+
+        /// <summary>
+        /// Rotates the given character c n positions through the alphabet whose first
+        /// letter is firstLetter and whose number of letters is alphabetLen. alphabetLen
+        /// must be positive.
+        /// </summary>
+        /// <param name="c">The character to rotate.</param>
+        /// <param name="n">The number of positions to rotate c.</param>
+        /// <param name="firstLetter">The first letter of the alphabet.</param>
+        /// <param name="alphabetLen">The number of letters in the alphabet.</param>
+        /// <returns>The result of the rotation.</returns>
+        private char Rotate(char c, int n, char firstLetter, int alphabetLen)
+        {
+            return (char)(firstLetter + (c - firstLetter + n) % alphabetLen);
+        }
+
+        /// <summary>
+        /// Encrypts the given character.
+        /// </summary>
+        /// <param name="c">The character to encrypt.</param>
+        /// <returns>The encrypted character.</returns>
+        private char Encrypt(char c)
+        {
+            if (c >= 'a' && c <= 'z')
+            {
+                return Rotate(c, 13, 'a', 26);
+            }
+            else if (c >= 'A' && c <= 'Z')
+            {
+                return Rotate(c, 13, 'A', 26);
+            }
+            else
+            {
+                return c;
+            }
+        }
+
+        /// <summary>
+        /// Handles a Click event on the "With String" menu item.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxEncryptWithString_Click(object sender, EventArgs e)
+        {
+            string text = uxEditBuffer.Text;
+            string result = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                result += Encrypt(text[i]);
+            }
+            uxEditBuffer.Text = result;
+        }
+
+        /// <summary>
+        /// Handles a Click event on the "With StringBuilder" menu item.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxEncryptWithStringBuilder_Click(object sender, EventArgs e)
+        {
+            string text = uxEditBuffer.Text;
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < text.Length; i++)
+            {
+                result.Append(Encrypt(text[i]));
+            }
+            uxEditBuffer.Text = result.ToString();
+        }
     }
 }
