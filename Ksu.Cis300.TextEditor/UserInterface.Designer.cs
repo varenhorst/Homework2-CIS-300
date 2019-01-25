7132bed9770c78cd13c6dd58aa1d191592a5e413
Rod Howell <rhowell@ksu.edu>
2019-01-25 11:46:42 -0600

    Lab 5 solution.

--- Ksu.Cis300.TextEditor/UserInterface.Designer.cs
+++ Ksu.Cis300.TextEditor/UserInterface.Designer.cs
@@ -32,12 +32,15 @@
             this.uxFile = new System.Windows.Forms.ToolStripMenuItem();
             this.uxOpen = new System.Windows.Forms.ToolStripMenuItem();
             this.uxSaveAs = new System.Windows.Forms.ToolStripMenuItem();
-            this.uxEditBuffer = new System.Windows.Forms.TextBox();
-            this.uxOpenDialog = new System.Windows.Forms.OpenFileDialog();
-            this.uxSaveDialog = new System.Windows.Forms.SaveFileDialog();
             this.uxEncrypt = new System.Windows.Forms.ToolStripMenuItem();
             this.uxEncryptWithString = new System.Windows.Forms.ToolStripMenuItem();
             this.uxEncryptWithStringBuilder = new System.Windows.Forms.ToolStripMenuItem();
+            this.uxEditBuffer = new System.Windows.Forms.TextBox();
+            this.uxOpenDialog = new System.Windows.Forms.OpenFileDialog();
+            this.uxSaveDialog = new System.Windows.Forms.SaveFileDialog();
+            this.uxEdit = new System.Windows.Forms.ToolStripMenuItem();
+            this.uxUndo = new System.Windows.Forms.ToolStripMenuItem();
+            this.uxRedo = new System.Windows.Forms.ToolStripMenuItem();
             this.uxMenuBar.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,7 +49,8 @@
             this.uxMenuBar.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.uxMenuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.uxFile,
-            this.uxEncrypt});
+            this.uxEncrypt,
+            this.uxEdit});
             this.uxMenuBar.Location = new System.Drawing.Point(0, 0);
             this.uxMenuBar.Name = "uxMenuBar";
             this.uxMenuBar.Size = new System.Drawing.Size(583, 33);
@@ -65,29 +69,16 @@
             // uxOpen
             // 
             this.uxOpen.Name = "uxOpen";
-            this.uxOpen.Size = new System.Drawing.Size(252, 30);
+            this.uxOpen.Size = new System.Drawing.Size(185, 30);
             this.uxOpen.Text = "Open . . .";
             this.uxOpen.Click += new System.EventHandler(this.uxOpen_Click);
             // 
             // uxSaveAs
             // 
             this.uxSaveAs.Name = "uxSaveAs";
-            this.uxSaveAs.Size = new System.Drawing.Size(252, 30);
+            this.uxSaveAs.Size = new System.Drawing.Size(185, 30);
             this.uxSaveAs.Text = "Save As . . .";
             this.uxSaveAs.Click += new System.EventHandler(this.uxSaveAs_Click);
-            // 
-            // uxEditBuffer
-            // 
-            this.uxEditBuffer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.uxEditBuffer.Location = new System.Drawing.Point(12, 36);
-            this.uxEditBuffer.MaxLength = 0;
-            this.uxEditBuffer.Multiline = true;
-            this.uxEditBuffer.Name = "uxEditBuffer";
-            this.uxEditBuffer.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.uxEditBuffer.Size = new System.Drawing.Size(559, 474);
-            this.uxEditBuffer.TabIndex = 1;
             // 
             // uxEncrypt
             // 
@@ -101,16 +92,57 @@
             // uxEncryptWithString
             // 
             this.uxEncryptWithString.Name = "uxEncryptWithString";
-            this.uxEncryptWithString.Size = new System.Drawing.Size(252, 30);
+            this.uxEncryptWithString.Size = new System.Drawing.Size(238, 30);
             this.uxEncryptWithString.Text = "With String";
             this.uxEncryptWithString.Click += new System.EventHandler(this.uxEncryptWithString_Click);
             // 
             // uxEncryptWithStringBuilder
             // 
             this.uxEncryptWithStringBuilder.Name = "uxEncryptWithStringBuilder";
-            this.uxEncryptWithStringBuilder.Size = new System.Drawing.Size(252, 30);
+            this.uxEncryptWithStringBuilder.Size = new System.Drawing.Size(238, 30);
             this.uxEncryptWithStringBuilder.Text = "With StringBuilder";
             this.uxEncryptWithStringBuilder.Click += new System.EventHandler(this.uxEncryptWithStringBuilder_Click);
+            // 
+            // uxEditBuffer
+            // 
+            this.uxEditBuffer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.uxEditBuffer.Location = new System.Drawing.Point(12, 36);
+            this.uxEditBuffer.MaxLength = 0;
+            this.uxEditBuffer.Multiline = true;
+            this.uxEditBuffer.Name = "uxEditBuffer";
+            this.uxEditBuffer.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.uxEditBuffer.Size = new System.Drawing.Size(559, 474);
+            this.uxEditBuffer.TabIndex = 1;
+            this.uxEditBuffer.TextChanged += new System.EventHandler(this.uxEditBuffer_TextChanged);
+            // 
+            // uxEdit
+            // 
+            this.uxEdit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uxUndo,
+            this.uxRedo});
+            this.uxEdit.Name = "uxEdit";
+            this.uxEdit.Size = new System.Drawing.Size(54, 29);
+            this.uxEdit.Text = "Edit";
+            // 
+            // uxUndo
+            // 
+            this.uxUndo.Enabled = false;
+            this.uxUndo.Name = "uxUndo";
+            this.uxUndo.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Z)));
+            this.uxUndo.Size = new System.Drawing.Size(252, 30);
+            this.uxUndo.Text = "Undo";
+            this.uxUndo.Click += new System.EventHandler(this.uxUndo_Click);
+            // 
+            // uxRedo
+            // 
+            this.uxRedo.Enabled = false;
+            this.uxRedo.Name = "uxRedo";
+            this.uxRedo.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Y)));
+            this.uxRedo.Size = new System.Drawing.Size(252, 30);
+            this.uxRedo.Text = "Redo";
+            this.uxRedo.Click += new System.EventHandler(this.uxRedo_Click);
             // 
             // UserInterface
             // 
@@ -141,6 +173,9 @@
         private System.Windows.Forms.ToolStripMenuItem uxEncrypt;
         private System.Windows.Forms.ToolStripMenuItem uxEncryptWithString;
         private System.Windows.Forms.ToolStripMenuItem uxEncryptWithStringBuilder;
+        private System.Windows.Forms.ToolStripMenuItem uxEdit;
+        private System.Windows.Forms.ToolStripMenuItem uxUndo;
+        private System.Windows.Forms.ToolStripMenuItem uxRedo;
     }
 }
 
--- Ksu.Cis300.TextEditor/UserInterface.cs
+++ Ksu.Cis300.TextEditor/UserInterface.cs
@@ -2,7 +2,7 @@
  * Author: Rod Howell
  */
 using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -19,6 +19,21 @@ namespace Ksu.Cis300.TextEditor
     /// </summary>
     public partial class UserInterface : Form
     {
+        /// <summary>
+        /// The history of edits that can be undone.
+        /// </summary>
+        private Stack _editHistory = new Stack();
+
+        /// <summary>
+        /// The history of undos that can be redone.
+        /// </summary>
+        private Stack _undoHistory = new Stack();
+
+        /// <summary>
+        /// The last text seen in the editor.
+        /// </summary>
+        private string _lastText = "";
+
         /// <summary>
         /// Constructs the GUI.
         /// </summary>
@@ -39,6 +54,11 @@ namespace Ksu.Cis300.TextEditor
                 try
                 {
                     uxEditBuffer.Text = File.ReadAllText(uxOpenDialog.FileName);
+                    _lastText = uxEditBuffer.Text;
+                    _editHistory.Clear();
+                    _undoHistory.Clear();
+                    uxUndo.Enabled = false;
+                    uxRedo.Enabled = false;
                 }
                 catch (Exception ex)
                 {
@@ -142,6 +162,160 @@ namespace Ksu.Cis300.TextEditor
                 result.Append(Encrypt(text[i]));
             }
             uxEditBuffer.Text = result.ToString();
+        }
+
+        /// <summary>
+        /// Records an edit made by the user.
+        /// </summary>
+        private void RecordEdit()
+        {
+            bool isDel = IsDeletion(uxEditBuffer, _lastText); // Indicates whether the edit was a deletion
+            int len = GetEditLength(uxEditBuffer, _lastText); // The length of the string inserted or deleted
+            int loc = GetEditLocation(uxEditBuffer, isDel, len); // The location of the edit
+            string text = uxEditBuffer.Text; // The current editor content
+            string editStr = GetEditString(text, _lastText, isDel, loc, len); // The string deleted or inserted
+            _lastText = text;
+            _editHistory.Push(isDel);
+            _editHistory.Push(loc);
+            _editHistory.Push(editStr);
+            _undoHistory.Clear();
+            uxUndo.Enabled = true;
+            uxRedo.Enabled = false;
+        }
+
+        /// <summary>
+        /// Returns whether text was deleted from the given string in order to obtain the contents
+        /// of the given TextBox.
+        /// </summary>
+        /// <param name="editor">The TextBox containing the result of the edit.</param>
+        /// <param name="lastContent">The string representing the text prior to the edit.</param>
+        /// <returns>Whether the edit was a deletion.</returns>
+        private bool IsDeletion(TextBox editor, string lastContent)
+        {
+            return editor.TextLength < lastContent.Length;
+        }
+
+        /// <summary>
+        /// Gets the length of the text inserted or deleted.
+        /// </summary>
+        /// <param name="editor">The TextBox containing the result of the edit.</param>
+        /// <param name="lastContent">The string representing the text prior to the edit.</param>
+        /// <returns>The length of the edit.</returns>
+        private int GetEditLength(TextBox editor, string lastContent)
+        {
+            return Math.Abs(editor.TextLength - lastContent.Length);
+        }
+
+        /// <summary>
+        /// Gets the location of the beginning of the edit.
+        /// </summary>
+        /// <param name="editor">The TextBox containing the result of the edit.</param>
+        /// <param name="isDeletion">Indicates whether the edit was a deletion.</param>
+        /// <param name="len">The length of the edit string.</param>
+        /// <returns>The location of the beginning of the edit.</returns>
+        private int GetEditLocation(TextBox editor, bool isDeletion, int len)
+        {
+            if (isDeletion)
+            {
+                return editor.SelectionStart;
+            }
+            else
+            {
+                return editor.SelectionStart - len;
+            }
+        }
+
+        /// <summary>
+        /// Gets the edit string.
+        /// </summary>
+        /// <param name="content">The current content of the TextBox.</param>
+        /// <param name="lastContent">The string representing the text prior to the edit.</param>
+        /// <param name="isDeletion">Indicates whether the edit was a deletion.</param>
+        /// <param name="editLocation">The location of the beginning of the edit.</param>
+        /// <param name="len">The length of the edit.</param>
+        /// <returns>The edit string.</returns>
+        private string GetEditString(string content, string lastContent, bool isDeletion, int editLocation, int len)
+        {
+            if (isDeletion)
+            {
+                return lastContent.Substring(editLocation, len);
+            }
+            else
+            {
+                return content.Substring(editLocation, len);
+            }
+        }
+
+        /// <summary>
+        /// Performs the given edit on the contents of the given TextBox.
+        /// </summary>
+        /// <param name="editor">The TextBox to edit.</param>
+        /// <param name="isDeletion">Indicates whether the edit is a deletion.</param>
+        /// <param name="loc">The location of the beginning of the edit.</param>
+        /// <param name="text">The text to insert or delete.</param>
+        private void DoEdit(TextBox editor, bool isDeletion, int loc, string text)
+        {
+            if (isDeletion)
+            {
+                _lastText = editor.Text.Remove(loc, text.Length);
+                editor.Text = _lastText;
+                editor.SelectionStart = loc;
+            }
+            else
+            {
+                _lastText = editor.Text.Insert(loc, text);
+                editor.Text = _lastText;
+                editor.SelectionStart = loc + text.Length;
+            }
+        }
+
+        /// <summary>
+        /// Handles a TextChanged event on the edit buffer.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxEditBuffer_TextChanged(object sender, EventArgs e)
+        {
+            if (uxEditBuffer.Modified)
+            {
+                RecordEdit();
+            }
+        }
+
+        /// <summary>
+        /// Handles a Click event on the Undo menu item.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxUndo_Click(object sender, EventArgs e)
+        {
+            string editStr = (string)_editHistory.Pop();
+            int loc = (int)_editHistory.Pop();
+            bool isDel = (bool)_editHistory.Pop();
+            _undoHistory.Push(isDel);
+            _undoHistory.Push(loc);
+            _undoHistory.Push(editStr);
+            DoEdit(uxEditBuffer, !isDel, loc, editStr);
+            uxRedo.Enabled = true;
+            uxUndo.Enabled = _editHistory.Count > 0;
+        }
+
+        /// <summary>
+        /// Handles a Click event on the Redo menu item.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxRedo_Click(object sender, EventArgs e)
+        {
+            string editStr = (string)_undoHistory.Pop();
+            int loc = (int)_undoHistory.Pop();
+            bool isDel = (bool)_undoHistory.Pop();
+            _editHistory.Push(isDel);
+            _editHistory.Push(loc);
+            _editHistory.Push(editStr);
+            DoEdit(uxEditBuffer, isDel, loc, editStr);
+            uxUndo.Enabled = true;
+            uxRedo.Enabled = _undoHistory.Count > 0;
         }
     }
 }
